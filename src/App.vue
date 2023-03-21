aba925ed9b6048bb1003a39447b9003cd3f91892
Maxime Moreillon <m.moreillon@gmail.com>
2023-03-21 12:09:11 +0900

    added login hint

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,19 @@
 <template>
-  <AppTemplate
-    :options="options"
-    @user="get_user($event)">
-
+  <AppTemplate :options="options" @user="get_user($event)">
     <template v-slot:nav>
-      <v-list
-        dense
-        nav >
+      <v-list dense nav>
         <v-list-item
           v-for="(item, index) in nav"
           :key="`nav_item_${index}`"
           :to="item.to"
-          exact>
+          exact
+        >
           <v-list-item-icon>
-            <v-icon>{{item.icon}}</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
 
           <v-list-item-content>
-            <v-list-item-title>{{item.title}}</v-list-item-title>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -26,29 +22,28 @@
     <template v-slot:footer>
       test
     </template>
-
   </AppTemplate>
 </template>
 
 <script>
-import AppTemplate from '@/AppTemplate.vue'
+import AppTemplate from "@/AppTemplate.vue"
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    AppTemplate
+    AppTemplate,
   },
 
   data: () => ({
     options: {
       // Names
       title: "Vuetify template",
-      author: 'Example Author', // name written in the footer
+      author: "Example Author", // name written in the footer
 
       // Colors
       colors: {
-        main: 'red',
-        footer: 'blue',
+        main: "red",
+        footer: "blue",
         //app_bar: 'yellow'
       },
       //main_class: 'blue lighten-4',
@@ -59,30 +54,34 @@ export default {
       //header_logo: require('@/assets/skt_logo_white.svg'),
       //authentication_logo: require('@/assets/skt_logo_black.svg'),
 
-      homepage_url: 'https://maximemoreillon.com',
-      profile_url: 'https://users.maximemoreillon.com/self',
+      homepage_url: "https://maximemoreillon.com",
+      profile_url: "https://users.maximemoreillon.com/self",
 
       // Authentication
       login_url: process.env.VUE_APP_LOGIN_URL,
       identification_url: process.env.VUE_APP_IDENTIFICATION_URL,
+      login_hint: "hello",
       // identification_url: 'http://172.16.98.151',
       password_reset_url: process.env.VUE_APP_PASSWORD_RESET_URL,
-      jwt_storage: 'localStorage',
-      drawer: false
+      jwt_storage: "localStorage",
+      drawer: false,
       //skip_greetings: true, // do not show greetings
-
     },
     nav: [
-      {title: 'Home', to: {name: 'Home'}, icon: 'mdi-home'},
-      {title: 'About', to: {name: 'About'}, icon: 'mdi-information-outline'},
-      {title: 'Misc', to: {name: 'Misc'}, icon: 'mdi-menu'},
-    ]
+      { title: "Home", to: { name: "Home" }, icon: "mdi-home" },
+      {
+        title: "About",
+        to: { name: "About" },
+        icon: "mdi-information-outline",
+      },
+      { title: "Misc", to: { name: "Misc" }, icon: "mdi-menu" },
+    ],
   }),
 
   methods: {
-    get_user(user){
+    get_user(user) {
       console.log(user)
-    }
-  }
-};
+    },
+  },
+}
 </script>
--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,48 +1,41 @@
 <template>
-<v-form
-  class="text-center"
-  @submit.prevent="login()">
+  <v-form class="text-center" @submit.prevent="login()">
+    <v-text-field
+      label="Username"
+      v-model="identifier"
+      prepend-icon="mdi-account"
+    />
 
-  <v-text-field
-    label="Username"
-    v-model="identifier"/>
+    <v-text-field
+      label="Password"
+      type="Password"
+      v-model="password"
+      prepend-icon="mdi-lock"
+    />
 
-  <v-text-field
-    label="Password"
-    type="Password"
-    v-model="password"/>
+    <v-btn dark type="submit" :loading="processing">
+      <v-icon left>mdi-login</v-icon>
+      <span>Login</span>
+    </v-btn>
 
-  <v-btn
-    dark
-    type="submit"
-    :loading="processing">
-    <v-icon left>mdi-login</v-icon>
-    <span>Login</span>
-  </v-btn>
+    <div v-if="options.password_reset_url" class="mt-5">
+      <a :href="options.password_reset_url">Password reset</a>
+    </div>
 
-  <div
-    v-if="options.password_reset_url"
-    class="mt-5">
-    <a :href="options.password_reset_url">Password reset</a>
-  </div>
+    <div v-if="options.login_hint" class="mt-5">
+      {{ options.login_hint }}
+    </div>
 
-  <v-snackbar
-      color="#C00000"
-      dark
-      v-model="snack" >
+    <v-snackbar color="#C00000" dark v-model="snack">
       {{ snack_text }}
 
       <template v-slot:action="{ attrs }">
-        <v-btn
-          icon
-          v-bind="attrs"
-          @click="snack = false" >
+        <v-btn icon v-bind="attrs" @click="snack = false">
           <v-icon>mdi-close</v-icon>
         </v-btn>
       </template>
     </v-snackbar>
-
-</v-form>
+  </v-form>
 </template>
 
 <script>
@@ -50,30 +43,28 @@
 This component exchanges credentials for a JWT and manages the storage of the JWT in cookies
 */
 
-import axios from 'axios'
-import VueCookie from 'vue-cookie'
-import StoreMixin from '../mixins/store.js'
+import axios from "axios"
+import VueCookie from "vue-cookie"
+import StoreMixin from "../mixins/store.js"
 
 export default {
-  name: 'LoginForm',
+  name: "LoginForm",
   props: {
     options: Object,
   },
   mixins: [StoreMixin],
-  components: {
-
-  },
-  data(){
+  components: {},
+  data() {
     return {
-      identifier: '',
-      password: '',
+      identifier: "",
+      password: "",
       snack: null,
-      snack_text: '',
+      snack_text: "",
       processing: false,
     }
   },
   methods: {
-    login(){
+    login() {
       // Send credentials and get JWT
 
       const url = this.options.login_url
@@ -82,54 +73,43 @@ export default {
       this.snack = null
       this.processing = true
 
-      axios.post(url, body)
+      axios
+        .post(url, body)
 
-      .then( ({data}) => {
+        .then(({ data }) => {
+          const { jwt } = data
 
-        const {jwt} = data
+          if (!jwt) return
 
-        if(!jwt) return
+          const cookie_options = {
+            secure: location.protocol === "https:",
+            samesite: "Strict",
+            expires: "1M",
+          }
 
-        const cookie_options = {
-          secure: location.protocol === 'https:',
-          samesite: 'Strict',
-          expires: '1M',
-        }
+          if (this.options.jwt_storage === "localStorage")
+            localStorage.setItem("jwt", jwt)
+          else VueCookie.set("jwt", jwt, cookie_options)
 
-        if (this.options.jwt_storage === 'localStorage') localStorage.setItem('jwt', jwt)
-        else VueCookie.set('jwt',jwt, cookie_options)
+          this.get_user()
 
-        this.get_user()
-
-        // clear the inputs
-        //this.identifier = ''
-        //this.password = ''
-      })
-      .catch( (error) => {
-        if(error.response) this.snack_text = error.response.data
-        else this.snack_text = `Error while logging in`
-        this.snack = true
-        console.error(error)
-       })
-      .finally(() => {
-
-        this.processing = false
-
-      })
-
+          // clear the inputs
+          //this.identifier = ''
+          //this.password = ''
+        })
+        .catch((error) => {
+          if (error.response) this.snack_text = error.response.data
+          else this.snack_text = `Error while logging in`
+          this.snack = true
+          console.error(error)
+        })
+        .finally(() => {
+          this.processing = false
+        })
     },
-
-
-
   },
-  computed: {
-
-  }
+  computed: {},
 }
 </script>
 
-<style scoped>
-
-
-
-</style>
+<style scoped></style>
